617af6a12e924058cc66646d3df2d0bb34f3df5e
johnbarneta <jgomez@ideosoft.es>
2020-06-15 12:46:07 +0200

    bump version

--- napalm_mikrotik/__init__.py
+++ napalm_mikrotik/__init__.py
@@ -1,14 +1,8 @@
 """napalm_mikrotik package."""
 
-# Import stdlib
-import pkg_resources
-
 # Import local modules
 from napalm_mikrotik.mikrotik import MikrotikDriver
 
-try:
-    __version__ = pkg_resources.get_distribution('napalm-mikrotik').version
-except pkg_resources.DistributionNotFound:
-    __version__ = "Not installed"
+__version__ = '0.6.0'
 
 __all__ = ('MikrotikDriver', )
